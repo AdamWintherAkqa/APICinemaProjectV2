69d43f1bb369dcf2087dc9ce5214dd78ff7b7c53
AdamWintherAkqa <100200760+AdamWintherAkqa@users.noreply.github.com>
2022-06-01 08:24:20 +0200

    filmcomponent init

--- CinemaProjectV2Frontend/CinemaProjectV2Frontend/src/app/app.module.ts
+++ CinemaProjectV2Frontend/CinemaProjectV2Frontend/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './components/homepage/homepage.component';
+import { FilmComponent } from './components/film/film.component';
 
 @NgModule({
-  declarations: [AppComponent, HomepageComponent],
+  declarations: [AppComponent, HomepageComponent, FilmComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
