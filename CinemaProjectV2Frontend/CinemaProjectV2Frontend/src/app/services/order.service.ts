4649ab6c120127ee46962a258a0e4942bbcf037e
AdamWintherAkqa <100200760+AdamWintherAkqa@users.noreply.github.com>
2022-06-02 13:22:54 +0200

    IOrder and service done

--- CinemaProjectV2Frontend/CinemaProjectV2Frontend/src/app/services/order.service.ts
+++ CinemaProjectV2Frontend/CinemaProjectV2Frontend/src/app/services/order.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import IOrder from '../interface/IOrder';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+};
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class OrderService {
+  baseUrl = 'https://localhost:44324/api/Movies';
+  constructor(private http: HttpClient) {} //DI
 
-  constructor() { }
+  getAllOrders(): Observable<IOrder[]> {
+    return this.http.get<IOrder[]>(this.baseUrl);
+  }
+  getOrderByID(id: number): Observable<IOrder> {
+    return this.http.get<IOrder>(`${this.baseUrl}/${id}`);
+  }
+  getEntireOrderByID(id: number): Observable<IOrder> {
+    return this.http.get<IOrder>(`${this.baseUrl}/GetEntireOrderByID/${id}`);
+  }
+  putOrder(order: IOrder): Observable<IOrder> {
+    return this.http.put<IOrder>(`${this.baseUrl}`, order);
+  }
+  postOrder(order: IOrder): Observable<IOrder> {
+    return this.http.post<IOrder>(`${this.baseUrl}`, order);
+  }
+  deleteOrder(id: number): Observable<IOrder> {
+    return this.http.delete<IOrder>(`${this.baseUrl}/${id}`);
+  }
 }
