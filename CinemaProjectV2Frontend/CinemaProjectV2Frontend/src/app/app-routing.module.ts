57d18325ce1be6188033c111378206d06aee49f3
AdamWintherAkqa <100200760+AdamWintherAkqa@users.noreply.github.com>
2022-06-07 12:59:58 +0200

    burde virke

--- CinemaProjectV2Frontend/CinemaProjectV2Frontend/src/app/app-routing.module.ts
+++ CinemaProjectV2Frontend/CinemaProjectV2Frontend/src/app/app-routing.module.ts
@@ -2,24 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './components/homepage/homepage.component';
-import { FilmComponent } from './components/film/film.component';
 import { AdminComponent } from './components/admin/admin.component';
 import { CreateMovieComponent } from './components/create-movie/create-movie.component';
-
+import { BookingComponent } from './components/booking/booking.component';
 
 const routes: Routes = [
   { path: '', component: HomepageComponent },
   // { path: 'film', component: FilmComponent },
-  { path: 'admin', component: AdminComponent},
-  { path: 'create-movie', component: CreateMovieComponent}
-
-
-
-const routes: Routes = [
-  { path: '', component: HomepageComponent },
-  { path: 'film', component: FilmComponent },
   { path: 'booking', component: BookingComponent },
-
+  { path: 'admin', component: AdminComponent },
+  { path: 'create-movie', component: CreateMovieComponent },
 ];
 
 @NgModule({
--- CinemaProjectV2Frontend/CinemaProjectV2Frontend/src/app/app.module.ts
+++ CinemaProjectV2Frontend/CinemaProjectV2Frontend/src/app/app.module.ts
@@ -5,15 +5,19 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './components/homepage/homepage.component';
-import { FilmComponent } from './components/film/film.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AdminComponent } from './components/admin/admin.component';
 import { CreateMovieComponent } from './components/create-movie/create-movie.component';
 import { BookingComponent } from './components/booking/booking.component';
 
 @NgModule({
-  declarations: [AppComponent, HomepageComponent, FilmComponent, BookingComponent, AdminComponent, CreateMovieComponent],
-
+  declarations: [
+    AppComponent,
+    HomepageComponent,
+    BookingComponent,
+    AdminComponent,
+    CreateMovieComponent,
+  ],
 
   imports: [
     BrowserModule,
