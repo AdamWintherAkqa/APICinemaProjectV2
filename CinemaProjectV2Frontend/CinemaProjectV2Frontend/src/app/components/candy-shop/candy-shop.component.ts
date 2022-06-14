93b4cf830bdbdb2973731396970699e232d65fd0
AdamWintherAkqa <100200760+AdamWintherAkqa@users.noreply.github.com>
2022-06-14 09:11:29 +0200

    candyShop init

---
+++ CinemaProjectV2Frontend/CinemaProjectV2Frontend/src/app/components/candy-shop/candy-shop.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-candy-shop',
+  templateUrl: './candy-shop.component.html',
+  styleUrls: ['./candy-shop.component.css']
+})
+export class CandyShopComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
