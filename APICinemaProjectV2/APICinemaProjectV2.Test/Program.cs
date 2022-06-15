812d4947598306ac24bf062fe4b998e7766f6384
AdamWintherAkqa <100200760+AdamWintherAkqa@users.noreply.github.com>
2022-06-15 10:32:30 +0200

    test init

---
+++ APICinemaProjectV2/APICinemaProjectV2.Test/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace APICinemaProjectV2.Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
