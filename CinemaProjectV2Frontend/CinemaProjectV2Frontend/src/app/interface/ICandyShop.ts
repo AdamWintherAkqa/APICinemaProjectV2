59671516fb5335a23cb55f140155c220f691483c
AdamWintherAkqa <100200760+AdamWintherAkqa@users.noreply.github.com>
2022-06-15 13:26:43 +0200

    candyshop init

--- CinemaProjectV2Frontend/CinemaProjectV2Frontend/src/app/interface/ICandyShop.ts
+++ CinemaProjectV2Frontend/CinemaProjectV2Frontend/src/app/interface/ICandyShop.ts
@@ -1,1 +1,9 @@
-export default interface ICandyShop {}
+import IOrder from './IOrder';
+
+export default interface ICandyShop {
+  candyShopID: number;
+  candyShopName: string;
+  candyShopPrice: number;
+  candyShopType: string;
+  orders: IOrder[];
+}
